dc5ba3d04bae662cb1e4a896c080e848af4ebf2f
Cao, Tong <tong.cao@cainiao.com>
2025-03-06 14:52:21 +0100

    feat: add trips fetching with rtk query

--- src/app/services/api/trip.ts
+++ src/app/services/api/trip.ts
@@ -1,4 +1,12 @@
-import { doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
 
 import type { Trip } from "@features/trip/type";
 import { auth, firestore } from "@services/firebase";
@@ -12,4 +20,38 @@ export async function addTrip(trip: Trip) {
     ...trip,
     userId: auth.currentUser.uid,
   });
+}
+
+export async function getTrips() {
+  if (!auth.currentUser) {
+    throw Error("You are not authorized to make this change");
+  }
+
+  const userTripsQuery = query(
+    collection(firestore, "trips"),
+    where("userId", "==", auth.currentUser.uid),
+  );
+
+  const querySnapshot = await getDocs(userTripsQuery);
+  return querySnapshot.docs.map((doc) => doc.data() as Trip);
+}
+
+export async function getTripById(tripId: string | undefined) {
+  if (!auth.currentUser) {
+    throw Error("You are not authorized to make this change");
+  }
+
+  if (!tripId) {
+    throw new Error("Trip not found!");
+  }
+
+  const tripRef = doc(firestore, "trips", tripId);
+
+  const tripSnap = await getDoc(tripRef);
+
+  if (!tripSnap.data()) {
+    throw new Error("Trip not found!");
+  }
+
+  return tripSnap.data() as Trip;
 }
